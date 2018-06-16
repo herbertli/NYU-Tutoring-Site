e0c06b4532431ca3d834dbb1494ff24d0baf48f6
Herbert Li <hl1785@nyu.edu>
2018-06-16 18:57:59 -0400

    Initial Commit

---
+++ src/components/Login.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const Login = () => {
+  return <Button bsStyle="primary" onClick={this.doLogin}>Login</Button>;
+}
+
+export default Login;
---
+++ src/components/PlayingScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const PlayingScreen = () => {
+  return <div>Playing Screen</div>;
+}
+
+export default PlayingScreen;
