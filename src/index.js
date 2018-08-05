73143c4760fcd64a148fbd006e744cac28fdde13
Herbert Li <hl1785@nyu.edu>
2018-08-05 14:50:28 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import React from 'react'
 import { render } from 'react-dom'
 
@@ -84,9 +85,16 @@ registerServiceWorker()
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+>>>>>>> initial commit
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+<<<<<<< HEAD
 >>>>>>> Initial commit
+=======
+>>>>>>> initial commit
