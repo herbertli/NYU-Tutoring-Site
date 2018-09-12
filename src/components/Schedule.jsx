50564e9de70cff8fbbb50df8108654693066de88
Herbert Li <hl1785@nyu.edu>
2018-09-12 01:17:37 -0400

    changed to hash router to fix gh-pages deployment, also cleaned up dependencies

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { Typography } from '@material-ui/core';
 
 const Schedule = () => {
-  return <div>Schedule</div>
+  return <div>
+    <Grid container>
+      <Grid item>
+
+      </Grid>
+      <Grid item>
+        <Typography>
+        </Typography>
+      </Grid>
+    </Grid>
+  </div>
 }
 
 export default Schedule;
